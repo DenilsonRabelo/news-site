50faf76636e4f68740b4bf09887f6d7c4dc1d499
Denilson <denilson2020@alu.ufc.br>
2022-02-27 22:38:51 -0300

    adicionando header e melhorando o visual

--- request.js
+++ request.js
@@ -10,9 +10,9 @@ fetch(req)
       for (var i in artigos){
         saida += `
         <li class="card">
-              <img src="${artigos[i].urlToImage}" class="card-img">
-              <h4>${artigos[i].title}</h4>
-              <p>${artigos[i].description}</p>
+            <div class=""conteiner-img><img src="${artigos[i].urlToImage}" class="card-img"></div> 
+            <div><h4>${artigos[i].title}</h4></div>
+            <div><p>${artigos[i].description}</p></div>
               <div class="btncc"><a target=_blank href="${artigos[i].url}" class=btn>Ler mais</a></div>
         </li>
         `
